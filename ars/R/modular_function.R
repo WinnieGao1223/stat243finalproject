01d553b855fe985b59597b878048f7c45bd434ec
WinnieGao <zhaochen_gao@berkeley.edu>
2018-12-11 18:00:40 -0800

    ars final version

--- ars/R/modular_function.R
+++ ars/R/modular_function.R
@@ -1,10 +1,9 @@
-
 #calculate the derivative of a function numerically
 #x is the point to evaluate the derivate at
 #fun is the log-concave function h
 #a is the lower bound, b is the upper bound
 #return the derivative approximately h'(x) at x
-Deriv_sub <- function(x, fun = h, a, b){
+Deriv_sub <- function(x, fun=h, a, b){
   if (x == a) {return ((fun(x + 1e-9)-fun(x))/1e-9)}
   if (x == b) {return ((fun(x) - fun(x - 1e-9))/1e-9)}
   if (a <= x && x <= b) {return((fun(x + 1e-9)-fun(x - 1e-9))/2e-9)}
@@ -16,19 +15,20 @@ Deriv_sub <- function(x, fun = h, a, b){
 #deriv is the derivative function
 #return a matrix including initial x, h(x), h'(x)
 init_mat <- function(a,b,fun,deriv){
+  #this mat stores x,f(x) and df(x)
   mat <- matrix(nrow = 2,ncol = 3)
   mat[1,1] <- a
   mat[1,2] <- fun(a)
   if(!is.finite(mat[1,2])){
     stop('a is not defined on function h', call. = FALSE)
   }
-  mat[1,3] <- deriv(a)
+  mat[1,3] <- deriv(a,fun,a,b)
   mat[2,1] <- b
   mat[2,2] <- fun(b)
   if(!is.finite(mat[2,2])){
     stop('b is not defined on function h', call. = FALSE)
   }
-  mat[2,3] <- deriv(b)
+  mat[2,3] <- deriv(b,fun,a,b)
   return(mat)
 }
 
@@ -46,7 +46,7 @@ init_z <- function(mat){
   z[1] <- mat[1,1]
   z[3] <- mat[2,1]
   #if the slopes at a and b are equal, set z as the mean of a and b
-  if (abs(mat[2,3]-mat[1,3]) < 1e-8){
+  if (abs(mat[2,3]-mat[1,3]) < 1e-9){
     z[2] <- (mat[2,1] + mat[1,1])/2
   } else {
     z[2] <- calc_z(mat)
@@ -54,21 +54,25 @@ init_z <- function(mat){
   return(z)
 }
 
+
 #mat is the ordered infor matrix
 #x_star is the sample needed to be updated
 #fun is the log-concave function h
 #deriv is the derivative function
 #retun updated infor matrix with ascending order of x
-update_mat <- function(mat,x_star,fun,deriv){
+update_mat <- function(mat,x_star,fun,deriv,a,b){
   index <- sum(mat[,1] < x_star) #find the location to insert x*
   tmp <- matrix(NA,1,3)
   mat <- rbind(mat,tmp)
   mat[(index+2):nrow(mat),] <- mat[(index+1):(nrow(mat)-1),]
-  mat[(index+1),] <- c(x_star,fun(x_star),deriv(x_star))
+  mat[(index+1),] <- c(x_star,fun(x_star),deriv(x_star,fun,a,b))
   return(mat)
 }
 
-
+#z is a ordered vector
+#mat is the updated infor matrix
+#x_star is the sample needed to be updated
+#retun the updated vector z given the updated infor matrix
 update_z <- function(z,x_star,mat){
   if (nrow(mat) <= 2){
     stop("something wrong with info matrix", call. = FALSE)
@@ -77,7 +81,7 @@ update_z <- function(z,x_star,mat){
   z <- c(z,NA)
   tmp_mat1 <- mat[(index-1):index,]
   tmp_mat2 <- mat[index:(index+1),]
-  if (sum(abs(diff(mat[(index-1):(index+1),3]))) < 1e-6){
+  if (sum(abs(diff(mat[(index-1):(index+1),3]))) < 1e-9){
     z1 <- mat[(index-1),1] + (mat[(index+1),1] - mat[(index-1),1])/3
     z2 <- mat[(index-1),1] + (mat[(index+1),1] - mat[(index-1),1])/3*2
   } else {
@@ -94,11 +98,30 @@ update_z <- function(z,x_star,mat){
   return(z)
 }
 
-#check if h'(x) decrease monotonically
-check_concave <- function(mat){
-  return(prod(mat[,3][-1] <= mat[,3][-nrow(mat)])==1)
+#z is the updated vector
+#mat is the updated infor matrix
+#n is the number of sample
+#return one sample
+sample_x <- function(z, mat, n=1){
+  uk1 <- exp(mat[,2]+(z[-1]-mat[,1])*mat[,3])
+  uk2 <- exp(mat[,2]+(z[-length(z)]-mat[,1])*mat[,3])
+  p <- rep(NA,nrow(mat))
+  p <- (uk1-uk2)/mat[,3]
+
+  # normalize p
+  p_norm <- p/sum(p)
+  w <- runif(n)
+  # determine the range we want to sample from
+  i <- sum(cumsum(p_norm) < w) + 1
+
+  # sample x using inverse cdf
+  samp_x <- (log(p[i]*mat[i,3]*runif(n)+exp(mat[i,2]+(z[i]-mat[i,1])*mat[i,3]))-mat[i,2])/mat[i,3]+mat[i,1]
+  return(samp_x)
 }
 
+#z is the updated vector
+#mat is the updated infor matrix
+#sample_x is the sample
 #calculate the upper bound
 upper_bound <- function(z, mat, samp_x){
   #compare samp_x with z to find which segment of line to calculate
@@ -107,6 +130,8 @@ upper_bound <- function(z, mat, samp_x){
   return(u)
 }
 
+#mat is the updated infor matrix
+#sample_x is the sample
 #calculate the lower bound
 lower_bound <- function(mat, samp_x){
   index <- rev(which(mat[,1] < samp_x))[1]
@@ -114,20 +139,7 @@ lower_bound <- function(mat, samp_x){
   return(l)
 }
 
-#returns a new sample given the info matrix and z
-sample_x <- function(z, mat, n=1){
-uk1 <- exp(mat[,2]+(z[-1]-mat[,1])*mat[,3])
-uk2 <- exp(mat[,2]+(z[-length(z)]-mat[,1])*mat[,3])
-p <- rep(NA,nrow(mat))
-p <- (uk1-uk2)/mat[,3]
-
-# normalize p
-p_norm <- p/sum(p)
-w <- runif(n)
-# determine the range we want to sample from
-i <- sum(cumsum(p_norm) < w) + 1
-
-# sample x using inverse cdf
-samp_x <- (log(p[i]*mat[i,3]*runif(n)+exp(mat[i,2]+(z[i]-mat[i,1])*mat[i,3]))-mat[i,2])/mat[i,3]+mat[i,1]
-return(samp_x)
+#check if h'(x) decrease monotonically
+check_concave <- function(mat){
+  return(prod(mat[,3][-1] <= mat[,3][-nrow(mat)])==1)
 }
--- ars/tests/testthat/test.R
+++ ars/tests/testthat/test.R
@@ -13,7 +13,7 @@ dgam <- function(x, theta=2, k=2){
 
 #pdf of beta(alpha=2,b=2)
 dbet <- function(x, alpha=2, b=2){
-return((x^(alpha-1)*(1-x)^(b-1))/beta(alpha, b))
+  return((x^(alpha-1)*(1-x)^(b-1))/beta(alpha, b))
 }
 
 #pdf of chi-square distribution
@@ -33,7 +33,7 @@ test_that("empirical distribution matches",{
   if (expect_that(ks_norm$p.value, function(x){x > 0.05}) == TRUE){
     print("passed with normal distribution")
   } else {print("failed to pass with normal distribution")}
-  
+
   #test gamma distribution
   print("testing gamma(shape=2,scale=2) distribution")
   samp_gamma <- ars(10000,dgam,a = 0.001, b = 1000)$sample
@@ -41,7 +41,7 @@ test_that("empirical distribution matches",{
   if (expect_that(ks_gamma$p.value, function(x){x > 0.05}) == TRUE){
     print("passed with gamma distribution")
   } else {print("failed to pass with gamma distribution")}
-  
+
   #test beta distribution
   print("testing beta(2,2) distribution")
   samp_beta <- ars(10000,dbet,a=0.01,b=0.99)$sample
@@ -49,7 +49,7 @@ test_that("empirical distribution matches",{
   if (expect_that(ks_beta$p.value,function(x){x>0.05}) == TRUE){
     print("passed with beta distribution")
   } else {print("failed to pass with beta distribution")}
-  
+
   #test chisq distribution
   print("testing chi-square distribution")
   samp_chi <- ars(10000,dchi,a = 0.01,b=100)$sample
@@ -57,11 +57,10 @@ test_that("empirical distribution matches",{
   if (expect_that(ks_chi$p.value,function(x){x>0.05}) == TRUE){
     print("passed with chisq distribution")
   } else {print("failed to pass with chisq distribution")}
-  
-  #test exp(x^2) distribution 
+
+  #test exp(x^2) distribution
   print("testing exp(x^2) distribution")
   if(inherits(try(ars(10000,dx2,a=-10,b=10)), "try-error")) {
     print(paste("fails to evaluate with exp(x^2) distribution"))
   }
-  })
-
+})
